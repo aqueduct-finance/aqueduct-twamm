a7152022d188f2098fa7f3572d7cad0bbb5fe7d7
JohnGuilding <jwguilding@gmail.com>
2023-07-13 19:03:13 +0100

    swap tuple values round and add comment

--- test/foundry/AqueductV1Pair.t.sol
+++ test/foundry/AqueductV1Pair.t.sol
@@ -109,8 +109,10 @@ contract AqueductV1PairIntegrationTest is AqueductTester {
         uint32 nextBlockTime = latestTime + 10;
 
         uint256 lpToken0Amount = 1 * 10 ** 18;
-        // FIXME: tuple values are other way round to typescript test??
-        (uint112 _reserve0New, uint112 _reserve1New) = aqueductV1Pair.getReservesAtTime(nextBlockTime);
+
+        // FIXME: tuple values are the other way round to typescript test because
+        // the factory sorts the addresses in ascending order when creating a pair
+        (uint112 _reserve1New, uint112 _reserve0New) = aqueductV1Pair.getReservesAtTime(nextBlockTime);
 
         // calculate correct ratio based on reserves
         uint256 lpToken1Amount = (_reserve1New * lpToken0Amount) / _reserve0New;
@@ -123,88 +125,6 @@ contract AqueductV1PairIntegrationTest is AqueductTester {
         vm.warp(nextBlockTime);
 
         uint256 expectedNewLiquidity = (lpToken1Amount * expectedInitialLiquidity) / _reserve1New;
-
-        uint256 expectedTotalLiquidity = expectedInitialLiquidity + expectedNewLiquidity;
-
-        aqueductV1Pair.mint(ALICE);
-
-        uint256 newTotalSupply = aqueductV1Pair.totalSupply();
-        // assertEq(newTotalSupply, expectedTotalLiquidity);
-
-        // uint256 aliceBalance = aqueductV1Pair.balanceOf(ALICE);
-        // assertEq(aliceBalance, expectedNewLiquidity);
-
-        console.log("_reserve0:                %s", _reserve0New);
-        console.log("_reserve1:                %s", _reserve1New);
-        console.log("____________________________");
-
-        console.log("lpToken1Amount:           %s", lpToken1Amount);
-        console.log("expectedInitialLiquidity: %s", expectedInitialLiquidity);
-        console.log("_reserve1:                %s", _reserve1New);
-        console.log("____________________________");
-
-        console.log("expectedInitialLiquidity: %s", expectedInitialLiquidity);
-        console.log("expectedNewLiquidity:     %s", expectedNewLiquidity);
-        console.log("newTotalSupply:           %s", newTotalSupply);
-        console.log("expectedTotalLiquidity:   %s", expectedTotalLiquidity);
-    }
-
-    function test_mint_position_and_provide_liquidity_WORKING() public {
-        uint256 expectedInitialLiquidity = 10 * 10 ** 18;
-
-        {
-            uint256 superTokenAAmount = 10 * 10 ** 18;
-            uint256 superTokenBAmount = 10 * 10 ** 18;
-
-            vm.startPrank(ADMIN);
-            addLiquidity(superTokenA, superTokenB, aqueductV1Pair, ADMIN, superTokenAAmount, superTokenBAmount);
-            vm.stopPrank();
-
-            uint256 totalSupply = aqueductV1Pair.totalSupply();
-            assertEq(totalSupply, expectedInitialLiquidity);
-
-            (uint112 _reserve0, uint112 _reserve1, uint time) = aqueductV1Pair.getRealTimeReserves();
-            assertEq(_reserve0, superTokenAAmount);
-            assertEq(_reserve1, superTokenBAmount);
-            assertEq(time, block.timestamp);
-        }
-
-        int96 flowRate = 1000000000;
-
-        vm.startPrank(ADMIN);
-        sf.host.callAgreement(
-            sf.cfa,
-            abi.encodeWithSelector(
-                sf.cfa.createFlow.selector,
-                superTokenA,
-                address(aqueductV1Pair),
-                flowRate,
-                new bytes(0) // placeholder - always pass in bytes(0)
-            ),
-            "0x" //userData
-        );
-        vm.stopPrank();
-
-        vm.warp(block.timestamp + 60);
-
-        uint32 latestTime = uint32(block.timestamp);
-        uint32 nextBlockTime = latestTime + 10;
-
-        uint256 lpToken0Amount = 1 * 10 ** 18;
-        (uint112 _reserve0New, uint112 _reserve1New) = aqueductV1Pair.getReservesAtTime(nextBlockTime);
-
-        // calculate correct ratio based on reserves
-        // FIXME: tuple values are other way round to typescript test. Swapped them round in this test so they pass
-        uint256 lpToken1Amount = (_reserve0New * lpToken0Amount) / _reserve1New;
-
-        vm.startPrank(ADMIN);
-        superTokenA.transfer(address(aqueductV1Pair), lpToken0Amount);
-        superTokenB.transfer(address(aqueductV1Pair), lpToken1Amount);
-        vm.stopPrank();
-
-        vm.warp(nextBlockTime);
-
-        uint256 expectedNewLiquidity = (lpToken1Amount * expectedInitialLiquidity) / _reserve0New;
         uint256 expectedTotalLiquidity = expectedInitialLiquidity + expectedNewLiquidity;
 
         aqueductV1Pair.mint(ALICE);
