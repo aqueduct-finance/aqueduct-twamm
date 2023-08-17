e7925b855bf6f8ce32a1fdfa613a83729ca136d1
JohnGuilding <jwguilding@gmail.com>
2023-08-17 14:58:50 +0100

    Update init code hash in library

--- src/libraries/AqueductV1Library.sol
+++ src/libraries/AqueductV1Library.sol
@@ -32,7 +32,7 @@ library AqueductV1Library {
                             bytes1(0xff),
                             factory,
                             keccak256(abi.encodePacked(token0, token1)),
-                            hex"661c737d5c0ceca23e53804ba51deab4c553843ad66883678db5fdc3a28b0563" // init code hash
+                            hex"58d4e73cdcb7da93613e90cc91a909bf6b0c8900bb499e8e96848a3a53ac17d2" // init code hash
                         )
                     )
                 )
