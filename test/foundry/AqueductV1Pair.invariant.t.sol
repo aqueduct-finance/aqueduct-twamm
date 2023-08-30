45e3961a8f454367507d7eeeb0ea31cf566a4667
JohnGuilding <jwguilding@gmail.com>
2023-08-30 21:56:41 +0100

    Call invariant test with funded accounts & valid super tokens

--- test/foundry/AqueductV1Pair.invariant.t.sol
+++ test/foundry/AqueductV1Pair.invariant.t.sol
@@ -22,12 +22,31 @@ import {StdUtils} from "forge-std/StdUtils.sol";
 contract AqueductV1PairHandler is CommonBase, StdCheats, StdUtils {
     using SuperTokenV1Library for ISuperToken;
 
+    address internal constant ADMIN = address(0x1);
+    address internal constant ALICE = address(0x2);
+    address internal constant BOB = address(0x3);
+    address internal constant CAROL = address(0x4);
+    address internal constant DAN = address(0x5);
+    address internal constant EVE = address(0x6);
+    address internal constant FRANK = address(0x7);
+    address internal constant GRACE = address(0x8);
+    address internal constant HEIDI = address(0x9);
+    address internal constant IVAN = address(0x10);
+
+    address[] internal testAccounts = [ADMIN, ALICE, BOB, CAROL, DAN, EVE, FRANK, GRACE, HEIDI, IVAN];
+
     AqueductV1Pair public aqueductV1Pair;
     AqueductV1Factory public aqueductV1Factory;
+
+    ISuperToken public superTokenA;
+    ISuperToken public superTokenB;
 
     constructor(address _feeToSetter, ISuperfluid _host, address tokenA, address tokenB) {
         aqueductV1Factory = new AqueductV1Factory(_feeToSetter, _host);
         aqueductV1Pair = AqueductV1Pair(aqueductV1Factory.createPair(tokenA, tokenB));
+
+        superTokenA = ISuperToken(tokenA);
+        superTokenB = ISuperToken(tokenB);
     }
 
     function mint(address to) public returns (uint256 liquidity) {
@@ -44,32 +63,68 @@ contract AqueductV1PairHandler is CommonBase, StdCheats, StdUtils {
 
         amount0Out = bound(amount0Out, 1, reserve0);
         amount1Out = bound(amount1Out, 1, reserve1);
+        vm.startPrank(address(aqueductV1Factory));
         aqueductV1Pair.swap(amount0Out, amount1Out, to);
+        vm.stopPrank();
     }
 
-    function retrieveFunds(ISuperToken superToken) public returns (uint256 returnedBalance) {
-        aqueductV1Pair.retrieveFunds(superToken);
+    function retrieveFundsTokenA() public returns (uint256 returnedBalance) {
+        aqueductV1Pair.retrieveFunds(superTokenA);
+    }
+
+    function retrieveFundsTokenB() public returns (uint256 returnedBalance) {
+        aqueductV1Pair.retrieveFunds(superTokenB);
     }
 
     function sync() public {
         aqueductV1Pair.sync();
     }
 
-    function createStream(ISuperToken superToken, address account) external {
-        vm.startPrank(account);
-        superToken.createFlow(address(aqueductV1Pair), 1 * 10 ** 18);
+    function createStreamTokenA(uint8 index) external {
+        index = uint8(bound(uint256(index), uint256(0), uint256(9)));
+
+        vm.startPrank(testAccounts[index]);
+        superTokenA.createFlow(address(aqueductV1Pair), 1 * 10 ** 18);
         vm.stopPrank();
     }
 
-    function updateStream(ISuperToken superToken, address account) external {
-        vm.startPrank(account);
-        superToken.updateFlow(address(aqueductV1Pair), 1 * 10 ** 18);
+    function createStreamTokenB(uint8 index) external {
+        index = uint8(bound(uint256(index), uint256(0), uint256(9)));
+
+        vm.startPrank(testAccounts[index]);
+        superTokenB.createFlow(address(aqueductV1Pair), 1 * 10 ** 18);
         vm.stopPrank();
     }
 
-    function deleteStream(ISuperToken superToken, address account) external {
-        vm.startPrank(account);
-        superToken.deleteFlow(account, address(aqueductV1Pair));
+    function updateStreamTokenA(uint8 index) external {
+        index = uint8(bound(uint256(index), uint256(0), uint256(9)));
+
+        vm.startPrank(testAccounts[index]);
+        superTokenA.updateFlow(address(aqueductV1Pair), 1 * 10 ** 18);
+        vm.stopPrank();
+    }
+
+    function updateStreamTokenB(uint8 index) external {
+        index = uint8(bound(uint256(index), uint256(0), uint256(9)));
+
+        vm.startPrank(testAccounts[index]);
+        superTokenB.updateFlow(address(aqueductV1Pair), 1 * 10 ** 18);
+        vm.stopPrank();
+    }
+
+    function deleteStreamTokenA(uint8 index) external {
+        index = uint8(bound(uint256(index), uint256(0), uint256(9)));
+
+        vm.startPrank(testAccounts[index]);
+        superTokenA.deleteFlow(testAccounts[index], address(aqueductV1Pair));
+        vm.stopPrank();
+    }
+
+    function deleteStreamTokenB(uint8 index) external {
+        index = uint8(bound(uint256(index), uint256(0), uint256(9)));
+
+        vm.startPrank(testAccounts[index]);
+        superTokenB.deleteFlow(testAccounts[index], address(aqueductV1Pair));
         vm.stopPrank();
     }
 }
@@ -86,13 +141,17 @@ contract AqueductV1PairInvariantTest is AqueductTester {
 
     function invariant_x_times_y_equals_k() public {
         // Arrange
-        (uint112 reserve0, uint112 reserve1, ) = aqueductV1PairHandler.aqueductV1Pair().getStaticReserves();
-        uint256 expectedKLast = reserve0 * reserve1;
+        (uint112 staticReserve0, uint112 staticReserve1, ) = aqueductV1PairHandler.aqueductV1Pair().getStaticReserves();
+        (uint112 realTimeReserve0, uint112 realTimeReserve1, ) = aqueductV1PairHandler.aqueductV1Pair().getReserves();
+
+        uint256 expectedKLastStaticReserves = staticReserve0 * staticReserve1;
+        uint256 expectedKLastRealTimeReserves = realTimeReserve0 * realTimeReserve1;
 
         // Act
         uint256 kLast = aqueductV1PairHandler.aqueductV1Pair().kLast();
 
         // Assert
-        assertEq(kLast, expectedKLast, "X * Y != K");
+        assertGe(expectedKLastStaticReserves, kLast, "X * Y != K");
+        assertGe(expectedKLastRealTimeReserves, kLast, "X * Y != K");
     }
 }
